d57a776513b869ce75bc9051ab4c1972a0c22eb8
Alison Reilly <reillyamr@gmail.com>
2020-10-04 15:36:00 -0400

    add view and edit Routes to thewall

--- src/containers/TheWall.js
+++ src/containers/TheWall.js
@@ -1,8 +1,12 @@
 //will render the message list component
 import React from 'react'
 import { connect } from 'react-redux'
+import { Route } from 'react-router-dom'
 import { getMessages } from '../store/actions/messageActions'
 import Messages from '../components/messages/Messages'
+import ViewMessage from '../components/messages/ViewMessage'
+import EditMessage from '../components/messages/EditMessage'
+
 
 class TheWall extends React.Component {
     componentDidMount(){
@@ -16,6 +20,18 @@ class TheWall extends React.Component {
             <div className="TheWall">
                 ---The Wall---
                 <Messages {...this.props} />
+
+                <Route exact path='/messages/:id' render={props => {
+                    const message = this.props.messages.find(element => element.id.toString() === props.match.params.id)
+                    return <ViewMessage message={message} {...props}/>
+                        }
+                    }/>
+                <Route exact path='/messages/:id/edit' render={props => {
+                    const message = this.props.messages.find(element => element.id.toString() === props.match.params.id)
+                    return <EditMessage message={message} {...props}/>
+                        }
+                }/>
+
             </div>
         );
   
