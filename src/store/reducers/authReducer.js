269398ab86644a65283de85ee67dffbf4ac7102e
Alison Reilly <reillyamr@gmail.com>
2020-10-04 17:28:47 -0400

    add authReducer

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,22 @@
+const initialState = {
+    token: null,
+    error: null, 
+    loading: false
+}
+
+export default function auth(state=initialState, action) {
+    switch (action.type) {
+        case 'AUTH_SUCCESS': 
+        return state, {
+            token: action.token,
+            error: null,
+            loading: false
+        };
+        case 'AUTH_LOGOUT':
+            return state, {
+                token: null
+            };
+        default:
+            return state;
+    }
+}
