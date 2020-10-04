5feea896f1ec1da45c63e8481cf9d1cacf23d8e5
Alison Reilly <reillyamr@gmail.com>
2020-10-04 10:46:08 -0400

    add composeEnhancers, store w thunk, export

--- src/store.js
+++ src/store.js
@@ -1,1 +1,6 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+export default store
