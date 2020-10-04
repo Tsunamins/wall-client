018e2454cb7c999f44459963766993ff9bdb446e
Alison Reilly <reillyamr@gmail.com>
2020-10-04 11:18:25 -0400

    add new reducer to a combined reducers in store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,10 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import messageReducer from './store/reducers/messageReducer'
+
+const reducer = combineReducers({
+    messageReducer
+ })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
