f5f49461595116f7ddf8c3e219b46451e078562b
Alison Reilly <reillyamr@gmail.com>
2020-10-04 15:35:43 -0400

    add edit and delete links

--- src/components/messages/ViewMessage.js
+++ src/components/messages/ViewMessage.js
@@ -1,18 +1,31 @@
 //will render message itself along with the 
 //User it belongs to
 import React from 'react';
-import { Link, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import {connect} from 'react-redux'
 
 function ViewMessage({message}) {
- console.log(message)
+
+  function handleDelete(){
+       
+    this.props.deletePost(this.props.message.id)
+
+  }
+     
+      console.log(message)
       return(
         message ?
         <div>
           <p>{message.content} </p>
           <p>{message.user}</p>
+          <div>
+              <Link to={`/messages/${message.id}/edit`}>Edit this Message</Link>
+          </div>
+          <div>
+              <button onClick={handleDelete}>Delete Message</button>
+          </div>
         </div> :
-        <div>Loc display issue</div>
+        <div>display issue</div>
       )
 
  
