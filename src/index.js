4170dc40d83cc2b1608b060d48951bd3ab74ff65
Alison Reilly <reillyamr@gmail.com>
2020-10-04 10:50:43 -0400

    import Provider and store.js, wrap App in Provider

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux'
 import {BrowserRouter as Router} from 'react-router-dom'
-
+import store from './store.js'
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
