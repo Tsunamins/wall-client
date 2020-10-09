bd8fe32d4977f14ce040202abffa5979b7263127
Alison Reilly <reillyamr@gmail.com>
2020-10-09 16:46:49 -0400

    add update message reducer

--- src/store/reducers/messageReducer.js
+++ src/store/reducers/messageReducer.js
@@ -5,6 +5,9 @@ export default function message(state = [], action) {
         return action.messages
       case 'ADD_MESSAGE':
         return state.concat(action.message)
+      case 'UPDATE_MESSAGE':
+        return state.map(message => message.id === action.message.id ? action.message : message)
+
 
         default:
             return state
