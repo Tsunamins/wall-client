7b558ceaba2f2b8ffb912e3d7a5a5629942d4839
Alison Reilly <reillyamr@gmail.com>
2020-10-09 12:24:28 -0400

    add create message fetch action

--- src/store/actions/messageActions.js
+++ src/store/actions/messageActions.js
@@ -16,6 +16,28 @@ export const getMessages = () => dispatch => {
 }
 
 
+export const createMessage = (message) => dispatch => {
+    return fetch('http://127.0.0.1:8000/wall-api/messages/create/', {
+        method: "POST",
+        headers: { 
+            "Content-Type": "application/json",
+            "Accept": 'application/json'
+        },
+        body: JSON.stringify(postData)
+        
+    })
+    .then(resp => resp.json())
+    .then(response => {
+        if(response.error){
+          alert(response.error)
+        } else {
+          console.log(response)
+          dispatch(addMessage(response.data))
+        }
+      })
+      .catch(console.log)
+}
+
 //ACTION CREATORS
 export const getAllMessages = messages => {    
     return {
@@ -23,3 +45,4 @@ export const getAllMessages = messages => {
         messages 
     }
   }
+
