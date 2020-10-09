0f5d9f810d3356837e194b7c29c53c1e86c59869
Alison Reilly <reillyamr@gmail.com>
2020-10-09 16:32:21 -0400

    import getCurrentUser func & map state to props

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import TheWall from './containers/TheWall'
 import Header from './containers/Header'
+import { getCurrentUser } from './store/actions/authActions'
 
 class App extends React.Component {
+  componentDidMount(){
+    this.props.getCurrentUser()
+  }
 
   render(){
-    console.log(this.props)
-
     return (
       <div className="App">
         The App
@@ -24,10 +26,10 @@ class App extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    isAuthenticated: state.authReducer.token !== null
+    isAuthenticated: state.authReducer.token !== null,
+    currentUser: state.authReducer.current
   }
 }
 
-// export default connect(null, mapDispatchToProps)(App);
-export default withRouter(connect(mapStateToProps)(App))
+export default withRouter(connect(mapStateToProps, {getCurrentUser})(App))
 
