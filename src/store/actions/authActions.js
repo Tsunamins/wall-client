04b26339fc1a39d9d3df7016162916abe4f38fa2
Alison Reilly <reillyamr@gmail.com>
2020-10-09 12:14:47 -0400

    add signup action

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -30,13 +30,40 @@ export const login = (creds) => {
     }
 }
 
-export const checkAuthTimeout = expirationTime => {
+
+export const signup = (creds) => {
+    console.log(creds)
+    
     return dispatch => {
-        setTimeout(() => {
-            dispatch(logout());
-        }, expirationTime * 1000)
+     
+        fetch('http://127.0.0.1:8000/rest-auth/registration/', {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            method: "POST",
+            body: JSON.stringify(creds)
+        })
+        .then(resp => resp.json())
+        .then(response => {
+            if(response.error){
+                alert(response.error)
+            } else {
+                console.log(response)
+                const token = response.key;
+                const expirationDate = new Date(new Date().getTime() + 3600 * 1000);
+                localStorage.setItem('token', token);
+                localStorage.setItem('expirationDate', expirationDate);
+                dispatch(authSuccess(token));
+                dispatch(checkAuthTimeout(3600));
+            }
+        })
+        .catch(console.log)
+        
     }
 }
+
+
 
 export const getCurrentUser = () => {
     return dispatch => {
@@ -66,6 +93,14 @@ export const authSuccess = token => {
     return {
         type: "AUTH_SUCCESS",
         token
+    }
+}
+
+export const checkAuthTimeout = expirationTime => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000)
     }
 }
 
